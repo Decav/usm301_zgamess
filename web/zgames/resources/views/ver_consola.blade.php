89acee3b26065dc16771113ce7ee1c0593a3df68
Diego Canelo Vigny <Dovahkiin509@gmail.com>
2021-06-10 20:17:29 -0400

    JS CSS

--- web/zgames/resources/views/ver_consola.blade.php
+++ web/zgames/resources/views/ver_consola.blade.php
@@ -1,5 +1,22 @@
 @extends("layouts.master")
 
 @section("contenido")
-    <h1>Hola esto es VER CONSOLA</h1>
+    <div class="row mt-5">
+        <div class="col-12 col-md-12 col-lg-6 mx-auto">
+            <table class="table table-hover table-bordered table-striped">
+                <thead class="bg-info">
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Marca</td>
+                        <td>Año de lanzamiento</td>
+                        <td>Acciones</td>
+                    </tr>
+                </thead>
+                <tbody id="tbody-consola">
+
+                </tbody>
+            </table>
+
+        </div>
+    </div>
 @endsection
